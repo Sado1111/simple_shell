f26a893915f02db4d2f54427f58981606db09df8
Sado1111 <sadohannahfabohns@gmail.com>
2023-09-17 22:55:05 +0100

    modified the simple shell program by adding more functions

--- sh.c
+++ sh.c
@@ -1,38 +1,154 @@
 #include "shell.h"
+
 /**
- * main - test parse.
- * @ac: argument count.
- * @av: argument vector.
- *
+ * run_external_cmd - executes external commands passed as input
+ * @args: array of arguments gotten from inputted command
+ * Return: exit status value of last command inputted
+ */
+int run_external_cmd(char *args[])
+{
+	int status;
+	pid_t cp = fork();
+
+
+	if (cp < 0)
+	{
+		fprintf(stderr, "Fork failed\n");
+		return (-1);
+	}
+	else if (cp == 0)
+	{
+		execvp(args[0], args);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(&status);
+		if (WIFEXITED(status))
+			return (WEXITSTATUS(status));
+	}
+
+	return (status);
+}
+
+/**
+ * find_and_execute - searches PATH for external executable commands
+ * @args: array of arguments from tokenized inputted command
+ * @shell_name: simple_shell's name
+ * @cmd_count: no. of commands inputted by user since shell was run
+ * Return: last command's exit status value to be passed in exit function
+ */
+
+int find_and_execute(char *args[], const char *shell_name, int cmd_count)
+{
+	char *path_env_var, *pathToken;
+	char path[KB_LENGTH], cmd_path[KB_LENGTH];
+	struct stat file_info;
+	int status = 0, found = 0;
+
+	path_env_var = getenv("PATH");
+
+	strcpy(path, path_env_var);
+
+	pathToken = strtok(path, ":");
+
+	if (stat(args[0], file_info) != 0)
+	{
+		while (pathToken != NULL)
+		{
+			strcpy(cmd_path, pathToken);
+			strcat(cmd_path, "/");
+			strcat(cmd_path, args[0]);
+
+			if (access(cmd_path, X_OK) == 0)
+			{
+				found = 1;
+				status = run_external_cmd(args);
+				break;
+			}
+			pathToken = strtok(NULL, ":");
+		}
+	}
+	else
+	{
+		found = 1;
+		status = run_external_cmd(args);
+	}
+	if (found != 1)
+	{
+		status = 127;
+		fprintf(stderr, "%s: %d: %s: not found\n", shell_name,
+				cmd_count, args[0]);
+	}
+	return (status);
+}
+
+/**
+ * check_N_execute - checks if args[0] is a built-in / external cmd
+ * @args: array of arguments from tokenized inputted command
+ * @shell_name: name of simple shell being run
+ * @cmd_count: no of commands inputted since shell has been run
+ * @status: last command's exit status
+ * @input: last command inputted by user to be tokenized and used
+ * Return: exit status stored in variable status
+ */
+
+int check_N_execute(char *args[], const char *shell_name,
+		int cmd_count, int status, char *input)
+{
+	if (strcmp(args[0], "exit") == 0)
+		status = shell_exit(args, shell_name, cmd_count, status, input);
+	else if (strcmp(args[0], "env") == 0)
+		status = print_env_variables(status);
+	else
+		status = find_and_execute(args, shell_name, cmd_count);
+}
+
+/**
+ * derive_ourShellName - uses getenv() to derive the shell name
+ * Return: pointer to char (strings) which the shell name derived
+ */
+
+char *derive_ourShellName()
+{
+	char *shell_name;
+
+	shell_name = getenv("_");
+	return (shell_name);
+}
+
+/**
+ * main -  entry point of the simple shell program
  * Return: Always 0.
  */
-int main(__attribute__((unused)) int ac, char **av)
+int main(void)
 {
-	pid_t cp;
-	char *tmp = NULL, **cm;
+	char *input = NULL, *args[KB_LENGTH];
 	size_t len = 0;
-	int i = 0;
+	const char *shell_name;
+	int cmd_count = 0, status = 0;
+
+	shell_name = derive_ourShellName();
 
 	while (1)
 	{
+		cmd_count++;
+
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "($) ", 4);
-		if (getline(&tmp, &len, stdin) == -1)
-			exit(0);
-		tmp[_strlen(tmp) - 1] = '\0';
-		cm = parse(tmp);
-		cp = fork();
-		if (cp == -1)
-			_perror("%s: ", av[0]);
-		if (cp == 0)
+		if (getline(&input, &len, stdin) == -1)
 		{
-			if (execve(cm[0], cm, environ) == -1)
-				_perror("%s: ", av[0]);
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, '\n', 1);
+			break;
 		}
-		else
-			wait(&i);
-		free_cm(cm);
-		free(tmp);
+
+		input[_strlen(tmp) - 1] = '\0';
+
+		parse_input(input, args);
+		if (args == NULL || args[0] == NULL)
+			continue;
+		status = check_N_execute(args, shell_name, cmd_count, status, input);
 	}
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -7,6 +7,9 @@ extern char **environ;
 /* Headers */
 #include <stdio.h>
 #include <unistd.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
@@ -14,6 +17,10 @@ extern char **environ;
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+
+/* Macros */
+#define KB_LENGTH 1024
+#define UNUSED(x) (void)(x)
 
 /* Functions */
 int _strlen(char *);
@@ -25,5 +32,18 @@ void _perror(const char *, ...);
 int _putchar(char);
 void print_str(va_list);
 char *_strdup(char *);
+int contains_non_digit(const char *s);
+int shell_exit(char *args[],
+		const char *shell_name, int cmd_count,
+		int status, char *input);
+int print_env_variables(int status);
+int run_external_cmd(char *args[]);
+int find_and_execute(char *args[],
+		const char *shell_name, int cmd_count);
+int check_N_execute(char *args[],
+		const char *shell_name, int cmd_count,
+		int status, char *input);
+char *derive_ourShellName();
+int parse_input(char *input, char *args[]);
 
 #endif
